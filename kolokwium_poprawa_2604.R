a6aa6acf8e5327e25906b26ce5f25cfa7a3a266d
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2022-04-25 19:03:34 +0200

    update

---
+++ kolokwium_poprawa_2604.R
@@ -0,0 +1,73 @@
+
+# 0. Zasady zaliczenia: Nowe technologie w badaniach społecznych ####
+# druga poprawa kolokwium 26. kwietnia 2022
+
+# Proszę przygotować działający skrypt w R, 
+# wykonując kolejne zadania. 
+# Skrypt ma działać w kolejności "z góry na dół" -
+# od pierwszej linii kodu do ostatniej.
+
+# Proszę nie zmieniać przygotowanych przeze mnie 
+# treści ani układu zadań (w komentarzach). 
+
+# Proszę zwrócić uwagę, jaki wynik należy przedstawić. 
+
+# Pracujemy na danych z "Diagnozy Społecznej", pakiet PogromcyDanych.
+
+# Powodzenia! RŻ
+
+########## Zadania ###########################################
+
+## 1. Proszę sprawdzić trendy wyszukiwania w wyszukiwarce Google ####
+# dla następujących socjologów i filozofów: 
+# Jürgen Habermas, Jacques Derrida, Pierre Bourdieu, Anthony Giddens
+# na całym świecie, ostatnie 5 lat, za pomocą Google Trends.
+
+# Wynik 1: proszę w komentarzu podać działający link do Google Trends,
+# z uzyskanymi wynikami.
+
+
+
+## 2. Proszę obliczyć statystyki opisowe #### 
+# dla pytania dotyczącego wagi respondentów w kilogramach.
+# Interesują nas: minimum, maksimum, mediana, 1. i 3. kwartyl, średnia
+# arytmetyczna i liczba braków danych. 
+
+# Wynik 2: proszę użyć jednej funkcji, wynik jej działania 
+# zawierający wszystkie wymagane statystyki proszę przypisać 
+# za pomocą <- do nazwy "wynik_2".
+
+
+
+## 3. Proszę przedstawić częstości odpowiedzi na pytanie #### 
+# "dzieci powinny pomagać rodzicom"
+
+# Wynik 3: proszę zaprezentować dane w tabeli i przypisać tabelę do 
+# nazwy "wynik_3".
+
+
+
+## 4. Proszę przedstawić zależność za pomocą wykresu pudełkowego ####
+# zmienna zależna: waga badanych w kilogramach, zmienna niezależna: płeć. 
+
+# Wynik 4: proszę przypisać wykres do nazwy "wynik_4"
+
+
+
+## 5. Proszę przedstawić zależność za pomocą dowolnego wykresu ####
+# interesuje nas to, czy odpowiedzi na pytanie "dzieci powinny pomagać rodzicom"
+# zależą od poziomu wykształcenia badanych. 
+
+# Wynik 5: za pomocą polecenia ggsave() proszę zapisać wykres do pliku .jpg
+# pod nazwą "nazwisko_wynik_5.jpg",
+# proszę wpisać swoje nazwisko.
+
+
+
+# Jak przesłać swoją pracę? ##############################################
+
+# Proszę o przesłanie do mnie na czacie MS Teams 
+# pliku skryptu w formacie .R, 
+# nazwa: nazwisko_kolokwium_poprawa_2604.R 
+
+# Dziękuję! 
