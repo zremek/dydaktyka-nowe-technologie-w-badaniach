33c08eab6f6fccc0652ab23771bf1817d3e0dbb9
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2023-01-20 16:01:02 +0100

    idea for next sem

---
+++ pomysl_2023.R
@@ -0,0 +1,8 @@
+# pomysł na zajęcia 2023:
+# zacząć bez pakietów, tylko base R wg. fastR
+# https://github.com/matloff/fasteR 
+# następnie tidyverse i dane gss_cat
+library(tidyverse)
+forcats::gss_cat %>% glimpse()
+## programowanie jest dla wszystkich o ile znają angielski :) 
+# potem PogromcyDanych
