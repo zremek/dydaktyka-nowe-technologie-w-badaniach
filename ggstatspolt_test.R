7fdbe5eb634a516dc476949a5309eaee8b1d735e
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-04-15 09:04:38 +0200

    testing third var

--- ggstatspolt_test.R
+++ ggstatspolt_test.R
@@ -3,6 +3,10 @@ library(ggstatsplot)
 
 library(PogromcyDanych)
 
-ggstatsplot::ggbarstats(data = diagnoza, x = gp3, y = plec)
+ggstatsplot::ggbarstats(data = diagnoza, x = gp3, y = plec) + 
+  coord_flip()
+
+ggstatsplot::grouped_ggbarstats(data = diagnoza, x = gp3, y = plec, 
+                                grouping.var = eduk4_2013)
 
 ggstatsplot::ggscatterstats(diagnoza, gp60, gp61)
