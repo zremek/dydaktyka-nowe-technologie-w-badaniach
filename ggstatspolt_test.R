c7401399cbe2c732dde35b62d750a363d17145cd
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-04-29 08:48:50 +0200

    density

--- ggstatspolt_test.R
+++ ggstatspolt_test.R
@@ -10,3 +10,9 @@ ggstatsplot::grouped_ggbarstats(data = diagnoza, x = gp3, y = plec,
                                 grouping.var = eduk4_2013)
 
 ggstatsplot::ggscatterstats(diagnoza, gp60, gp61)
+
+
+ggplot(diagnoza, aes(x = gp60, colour = plec)) + geom_density() + 
+  scale_x_continuous(breaks = seq(from = 0, to = 300, by = 10))
+
+by(data = diagnoza$gp60, INDICES = diagnoza$plec, FUN = summary)
