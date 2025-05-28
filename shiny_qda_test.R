c1370dec7fa241d924bafb5cf0827be82fc2b93d
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-05-28 14:30:12 +0200

    add url

--- shiny_qda_test.R
+++ shiny_qda_test.R
@@ -1,3 +1,5 @@
+# https://jbryer.github.io/ShinyQDA/ 
+
 # remotes::install_github('jbryer/ShinyQDA', dependencies = c('Depends', 'Imports', 'Suggests'))
 
 shiny::runApp(paste0(find.package('ShinyQDA'), '/daacs_demo/'))
